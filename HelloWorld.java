5a41c03408175a7bece0ebd7de942083db892c42
Ravikiranmasule <ravikiran.masule@gmail.com>
2025-11-01 01:30:47 +0530

    added second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from DITISS!");
+        System.out.println("Hello from Heaven!");
+System.out.println("This is a second hello line.");
     }
 }
